d6fa5b3d19c212efba9a2f396d6e9cec4a5cd96c
appoclayptor <shahreza1414@gmail.com>
2024-11-03 10:59:29 +0600

    first commit

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,11 @@
+
+const Error = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">Page Not Found</h2>
+            <p>Status: 404</p>
+        </div>
+    );
+};
+
+export default Error;
